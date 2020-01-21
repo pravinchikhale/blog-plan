f32df09ae058be3c980a1f611ff46585ecef1b30
pravinchikhale <pravin.chikhale1@gmail.com>
2020-01-21 20:58:30 +0530

    Initial commit

---
+++ calendar-service/src/test/java/org/blogplan/CalendarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blogplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
